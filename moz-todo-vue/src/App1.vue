c61f80b924cdefab8ec83fb00010892f8c352489
f2xiao <fxiao.mecheng@gmail.com>
2022-01-07 00:51:52 -0500

    Add this.()

--- moz-todo-vue/src/App1.vue
+++ moz-todo-vue/src/App1.vue
@@ -1,20 +1,42 @@
 <template>
   <div id="app">
-    <p>App Root Comp</p>
+    <h1 ref="myh1">App Root Comp</h1>
     <button @click="showThis">Print this</button>
+    <button @click="onReset">Reset the count on Left Component </button>
     <hr>
-   
+    <input type="text" v-if="inputVisible" ref="ipt">
+    <button v-else @click="showInput">Show input</button>
+    <hr>
+   <Left ref="comLeft"></Left>
   </div>
 </template>
 
 <script>
-
+import Left from '@/components/Left.vue'
 export default {
+  data(){
+    return {
+      inputVisible: false
+    }
+  },
   methods: {
     showThis(){
       console.log(this);
-    }
+      this.$refs.myh1.style.color = 'red';
+    },
+    onReset(){
+      this.$refs.comLeft.countFromSon=0;
+    },
+    showInput(){
+      this.inputVisible = true;
+     this.$nextTick(()=>{
+        this.$refs.ipt.focus();
+     })
+    },
   },
+  components:{
+    Left
+  }
 };
 </script>
 
