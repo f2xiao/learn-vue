4ec4b482a58b9b65e65bc2597b388f167f975dce
f2xiao <fxiao.mecheng@gmail.com>
2022-01-07 00:20:05 -0500

    Add App2.vue

---
+++ moz-todo-vue/src/App1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <p>App Root Comp</p>
+    <button @click="showThis">Print this</button>
+    <hr>
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    showThis(){
+      console.log(this);
+    }
+  },
+};
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
