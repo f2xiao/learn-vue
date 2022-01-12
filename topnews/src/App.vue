6b15569017a2caf111c2dd3dfd60355f0434cd02
f2xiao <fxiao.mecheng@gmail.com>
2022-01-12 11:36:18 -0500

    Add router

--- topnews/src/App.vue
+++ topnews/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>App Root Comp</h1>
+    <router-link to="/home">Home</router-link>
+    <router-link to="/user">User</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style lang="less">
