358e575c6197ff3bf2833d7ee74c13bfd3f974cc
f2xiao <fxiao.mecheng@gmail.com>
2022-01-20 13:00:46 -0500

    Add Person component

---
+++ basics-vue/src/components/Person.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Person List</h1>
+    <input v-model="name" type="text" placeholder="Type a person's name" />
+    <button @click="addPerson">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Person",
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
+</script>
