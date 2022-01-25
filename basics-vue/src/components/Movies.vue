dfc9873d56158938d420bd87d32c0f14b0170039
f2xiao <fxiao.mecheng@gmail.com>
2022-01-25 11:59:13 -0500

    Edit the movies page

--- basics-vue/src/components/Movies.vue
+++ basics-vue/src/components/Movies.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="movies-container">
     <h1>Movies</h1>
-    <input v-model="movie" type="text" />
+    <input v-model="movie.name" type="text" />
     <button @click="addMovie">Add a movie</button>
     <button @click="deleteMovie">Delete a movie</button>
+    <button @click="modifyMovies">Modify a movie</button>
     <ul>
       <li v-for="movie in movies" :key="movie.id">
         <router-link :to="`/movies/${movie.id}`">{{ movie.name }}</router-link>
@@ -22,19 +23,24 @@ export default {
         { id: 2, name: "Captain American" },
         { id: 3, name: "Hulk" },
       ],
-      movie: "",
+      movie: { id: 4 },
     };
   },
   methods: {
     addMovie() {
-      const id = this.movies.length + 1;
-      const movie = { id: id, name: `${this.movie}` };
-      console.log(movie);
-      this.movies.push(movie);
-      this.movie = "";
+      console.log(this.movie);
+      this.movie["name"] = "Alice in Wonderland";
+      console.log(this.movie);
+      this.movies.push(this.movie);
+    },
+    modifyMovies() {
+      // Vue 2 can't react to the data change of the new added property in an object
+      this.movie.name = "Black Widow";
+      // this.movies[1].name = "Alice in Wonderland";
     },
     deleteMovie() {
-      console.log(this.movie);
+      // Vue 2 can't react to the data change of the new added property in an object
+      this.movie.name = "";
     },
   },
 };
