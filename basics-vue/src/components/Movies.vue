725d4aa808e6af3d87b104296cfeead116267d1e
f2xiao <fxiao.mecheng@gmail.com>
2022-01-25 11:35:22 -0500

    Add two buttons to modify the movies listed

--- basics-vue/src/components/Movies.vue
+++ basics-vue/src/components/Movies.vue
@@ -1,19 +1,43 @@
 <template>
   <div class="movies-container">
     <h1>Movies</h1>
+    <input v-model="movie" type="text" />
+    <button @click="addMovie">Add a movie</button>
+    <button @click="deleteMovie">Delete a movie</button>
     <ul>
-      <li><router-link to="/movies/1">Loki</router-link></li>
-      <li><router-link to="/movies/2">Captain American</router-link></li>
-      <li>
-        <router-link to="/movies/3">Hulk</router-link>
-        <router-view></router-view>
+      <li v-for="movie in movies" :key="movie.id">
+        <router-link :to="`/movies/${movie.id}`">{{ movie.name }}</router-link>
       </li>
+      <router-view></router-view>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      movies: [
+        { id: 1, name: "Loki" },
+        { id: 2, name: "Captain American" },
+        { id: 3, name: "Hulk" },
+      ],
+      movie: "",
+    };
+  },
+  methods: {
+    addMovie() {
+      const id = this.movies.length + 1;
+      const movie = { id: id, name: `${this.movie}` };
+      console.log(movie);
+      this.movies.push(movie);
+      this.movie = "";
+    },
+    deleteMovie() {
+      console.log(this.movie);
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
