28a1bb891df4c26b43c3dafa47cd37d14f046085
f2xiao <fxiao.mecheng@gmail.com>
2022-01-25 23:20:44 -0500

    Add Cat component

---
+++ learn-vue3/src/components/Cat.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Cat's Info</h1>
+  <p>{{ name }}</p>
+  <p>{{ age }}</p>
+  <ul>
+    <li v-for="hobby in hobbies">{{ hobby }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  setup() {
+    let data = {
+      name: "Pixie",
+      age: 8,
+      hobbies: ["eat", "drink", "play balls"],
+    };
+
+    return data;
+  },
+};
+</script>
+
+<style></style>
