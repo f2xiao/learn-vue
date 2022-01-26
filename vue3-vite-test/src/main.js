8d195baa99f80723514f36863bf5fc20e14e04eb
f2xiao <fxiao.mecheng@gmail.com>
2022-01-25 23:00:24 -0500

    Add learn-vue3

--- vue3-vite-test/src/main.js
+++ vue3-vite-test/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { createApp } from "vue";
+// import { Vue } from "vue";
+import App from "./App.vue";
+import "./index.css";
 
-createApp(App).mount('#app')
+// return a lighter version of VueComponent compared to Vue2
+console.log(createApp(App));
+
+createApp(App).mount("#app");
+// new Vue({
+//   render: (h) => h(App),
+// }).mount("#app");
